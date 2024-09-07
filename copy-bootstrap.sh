5ebf72ff2305cd5c0fd1e2ad4185ff7a1c207a8a
Andressa Guimaraes <andressa.guimaraes2171@gmail.com>
2024-09-07 21:02:50 +0200

    First commit docker, twig, first draft of form, bootstrap

---
+++ copy-bootstrap.sh
@@ -0,0 +1,2 @@
+mkdir -p public/static/bootstrap
+cp -R vendor/twbs/bootstrap/dist public/static/bootstrap
---
+++ src/Controller/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\FizzBuzzType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/index', name: 'app_index')]
+    public function index(): Response
+    {
+
+        $form =$this->createForm(FizzBuzzType::class);
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+            'form' => $form->createView(),
+        ]);
+    }
+}
